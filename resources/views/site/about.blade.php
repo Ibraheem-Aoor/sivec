b0461e884828c40fbefdc20d6a22be280c0f19f6
Ibraheem <ibraheem.alaoor@hotmail.com>
2023-01-29 16:20:34 +0200

    up

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -10,10 +10,16 @@
                 <div class="col-md-12 col-lg-8 col-xl-6 wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
                     <div class="about-image-box-style1 about-side-line mrr-60 mrr-lg-0">
                         <figure class="about-image1 js-tilt d-none d-md-block d-lg-block d-xl-block">
-                            <img class="img-full"  src="{{Storage::url("site/about/{@$page_settings['about_image_2']}")}}">
+                            @if (@$page_settings['about_image_2'])
+                                <img class="img-full"
+                                    src="{{ Storage::url("site/about/{$page_settings['about_image_2']}") }}">
+                            @endif
                         </figure>
                         <figure class="about-image2">
-                            <img class=" img-full" src="{{Storage::url("site/about/{@$page_settings['about_image_1']}")}}" alt="">
+                            @if (@$page_settings['about_image_1'])
+                                <img class=" img-full"
+                                    src="{{ Storage::url("site/about/{$page_settings['about_image_1']}") }}" alt="">
+                            @endif
                         </figure>
                     </div>
                 </div>
@@ -59,7 +65,7 @@
                                 </div>
                                 <div class="featured-content">
                                     <h4 class="featured-title">Exclusive Design</h4>
-                                    <p class="featured-desc">{{$page_settings['exclusive_design_description']}}</p>
+                                    <p class="featured-desc">{{ $page_settings['exclusive_design_description'] }}</p>
                                 </div>
                             </div>
                             <div class="featured-icon-box mrb-sm-40">
@@ -68,7 +74,7 @@
                                 </div>
                                 <div class="featured-content">
                                     <h4 class="featured-title">Professional Team</h4>
-                                    <p class="featured-desc mrb-0">{{$page_settings['pro_team_description']}}</p>
+                                    <p class="featured-desc mrb-0">{{ $page_settings['pro_team_description'] }}</p>
                                 </div>
                             </div>
                         </div>
