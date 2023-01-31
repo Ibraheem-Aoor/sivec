6c6f9a837f0e338fb3f053b5b6a0270930230379
Ibraheem <ibraheem.alaoor@hotmail.com>
2023-01-31 23:36:09 +0200

    up

--- app/Transformers/ServiceCategoryTransformer.php
+++ app/Transformers/ServiceCategoryTransformer.php
@@ -23,9 +23,9 @@ class ServiceCategoryTransformer extends TransformerAbstract
     public function getActionButtons($service_category)
     {
         return "<button class='btn-xs btn-success'  data-toggle='modal' data-target='#service-category-create-update-modal'
-        data-action='".route('admin.job-title.custom_update' , $service_category->id)."' data-method='POST' data-service-category='".json_encode($service_category)."' data-is-create='false'><i class='fa fa-edit'></i></button>
+        data-action='".route('admin.service-category.custom_update' , $service_category->id)."' data-method='POST' data-service-category='".json_encode($service_category)."' data-is-create='false'><i class='fa fa-edit'></i></button>
         <button type='button' data-toggle='modal' data-target='#delete-modal' class='btn-xs btn-danger'
-        data-delete-url='".route('admin.job-title.destroy' , $service_category->id)."' data-message='".__('custom.confirm_delete')."' data-name='".$service_category->name."' id='row-".$service_category->id."'><i class='fa fa-trash'></i></button>
+        data-delete-url='".route('admin.service-category.destroy' , $service_category->id)."' data-message='".__('custom.confirm_delete')."' data-name='".$service_category->name."' id='row-".$service_category->id."'><i class='fa fa-trash'></i></button>
         ";
     }
 }
--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -12,21 +12,9 @@
     <link rel="stylesheet" href="{{ asset('admin_assets/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Tempusdominus Bbootstrap 4 -->
-    <link rel="stylesheet"
-        href="{{ asset('admin_assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }}">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="{{ asset('admin_assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-    <!-- JQVMap -->
-    <link rel="stylesheet" href="{{ asset('admin_assets/plugins/jqvmap/jqvmap.min.css') }}">
+    
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('admin_assets/dist/css/adminlte.min.css') }}">
-    <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="{{ asset('admin_assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{ asset('admin_assets/plugins/daterangepicker/daterangepicker.css') }}">
-    <!-- summernote -->
-    <link rel="stylesheet" href="{{ asset('admin_assets/plugins/summernote/summernote-bs4.css') }}">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
@@ -139,7 +127,7 @@
 
 
         <footer class="main-footer">
-            <strong>Copyright &copy; 2023 <a href="{{route('admin.dashboard')}}">SIVEC</a>.</strong>
+            <strong>Copyright &copy; 2023 <a href="{{ route('admin.dashboard') }}">SIVEC</a>.</strong>
             All rights reserved.
             <div class="float-right d-none d-sm-inline-block">
                 <b>Version</b> 1.0.0
@@ -158,8 +146,6 @@
 
     <!-- jQuery -->
     <script src="{{ asset('admin_assets/plugins/jquery/jquery.min.js') }}"></script>
-    <!-- jQuery UI 1.11.4 -->
-    <script src="{{ asset('admin_assets/plugins/jquery-ui/jquery-ui.min.js') }}"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
         $.widget.bridge('uibutton', $.ui.button)
@@ -172,30 +158,10 @@
     <script src="{{ asset('admin_assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- ChartJS -->
     <script src="{{ asset('admin_assets/plugins/chart.js/Chart.min.js') }}"></script>
-    <!-- Sparkline -->
-    {{-- <script src="{{ asset('admin_assets/plugins/sparklines/sparkline.js') }}"></script> --}}
-    <!-- JQVMap -->
-    {{-- <script src="{{ asset('admin_assets/plugins/jqvmap/jquery.vmap.min.js') }}"></script> --}}
-    {{-- <script src="{{ asset('admin_assets/plugins/jqvmap/maps/jquery.vmap.world.js') }}"></script> --}}
-    <!-- jQuery Knob Chart -->
-    <script src="{{ asset('admin_assets/plugins/jquery-knob/jquery.knob.min.js') }}"></script>
-    <!-- daterangepicker -->
-    <script src="{{ asset('admin_assets/plugins/moment/moment.min.js') }}"></script>
-    <script src="{{ asset('admin_assets/plugins/daterangepicker/daterangepicker.js') }}"></script>
-    <!-- Tempusdominus Bootstrap 4 -->
-    <script src="{{ asset('admin_assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js') }}">
-    </script>
-    <!-- Summernote -->
-    <script src="{{ asset('admin_assets/plugins/summernote/summernote-bs4.min.js') }}"></script>
-    <!-- overlayScrollbars -->
-    <script src="{{ asset('admin_assets/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
     <!-- AdminLTE App -->
     <script src="{{ asset('admin_assets/dist/js/adminlte.js') }}"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
     <script src="{{ asset('admin_assets/dist/js/pages/dashboard.js') }}"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="{{ asset('admin_assets/dist/js/demo.js') }}"></script>
-
     {{-- Toastr js --}}
     <script src="{{ asset('admin_assets/plugins/toastr/toastr.min.js') }}"></script>
     <script>
@@ -206,7 +172,7 @@
         });
     </script>
 
-    <script src="{{ asset('admin_assets/dist/js/custom/master.js') }}"></script>
+    <script src="{{ asset('admin_assets/dist/js/custom/master.js?v=0.01') }}"></script>
     @stack('js')
 
 </body>
