7972c1d34b8b9a6683481caa08f4ea69e52c0e3e
Ibraheem <ibraheem.alaoor@hotmail.com>
2023-05-30 15:27:53 +0300

    up

--- resources/views/layouts/user/master.blade.php
+++ resources/views/layouts/user/master.blade.php
@@ -398,7 +398,7 @@
     </script>
 
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LZWVDK3KPK"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K3TSLKMVVE"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
