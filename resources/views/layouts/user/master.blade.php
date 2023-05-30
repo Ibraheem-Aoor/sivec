f4aa647fa9ef545e550462249976ee9469646c61
Ibraheem <ibraheem.alaoor@hotmail.com>
2023-05-30 15:40:18 +0300

    up

--- resources/views/layouts/user/master.blade.php
+++ resources/views/layouts/user/master.blade.php
@@ -397,18 +397,6 @@
         })
     </script>
 
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K3TSLKMVVE"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-LZWVDK3KPK');
-    </script>
     @stack('js')
 </body>
 
