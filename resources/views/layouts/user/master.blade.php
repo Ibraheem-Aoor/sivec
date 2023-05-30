5e94174e920a96aedc5714892898673f628f073e
Ibraheem <ibraheem.alaoor@hotmail.com>
2023-05-30 15:13:36 +0300

    up

--- resources/views/layouts/user/master.blade.php
+++ resources/views/layouts/user/master.blade.php
@@ -396,6 +396,19 @@
             $('#catalogModal').modal('hide');
         })
     </script>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LZWVDK3KPK"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-LZWVDK3KPK');
+    </script>
     @stack('js')
 </body>
 
